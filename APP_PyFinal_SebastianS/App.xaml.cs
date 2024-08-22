a1efe18f3741cc767555ce725873602a26cd07ba
Sebastian <seniormonolito@gmail.com>
2024-08-21 20:55:06 -0600

    Add project files.

---
+++ APP_PyFinal_SebastianS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace APP_PyFinal_SebastianS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ APP_PyFinal_SebastianS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace APP_PyFinal_SebastianS
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
